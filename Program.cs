8fbc344f6c618f973404f234d89709e074e15160
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-02-18 20:21:59 -0300

    18/02 20:21

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstraindo_um_jogo_de_RPG_Usando_Orientacao_a_Objetos_com_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Entities/Arus.cs
@@ -0,0 +1,7 @@
+namespace Abstraindo_um_jogo_de_RPG_Usando_Orientacao_a_Objetos_com_C_sharp.src.Entities
+{
+    public class Arus
+    {
+        
+    }
+}
